27227969f713967b094b74a232897e4c538e5f9f
Luis Colorado <lcu@oddsreactor.com>
2020-09-21 21:25:56 +0300

    Add srv to .gitignore and add usage.h (forgot last commit)
    Signed-off-by: Luis Colorado <lcu@oddsreactor.com>

---
+++ usage.h
@@ -0,0 +1,12 @@
+/* usage.h -- usage interface.
+ * Author: Luis Colorado <luiscoloradourcola@gmail.com>
+ * Date:
+ * Copyright: (C) 2005-2020 Luis Colorado.  All rights reserved.
+ * License: BSD.
+ */
+#ifndef _USAGE_H
+#define _USAGE_H
+
+void do_usage(char *prog);
+
+#endif /* _USAGE_H */
